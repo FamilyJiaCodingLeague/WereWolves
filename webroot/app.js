b484872d51c8149aff3ccbe2fc392e8342e261c4
Alex Yang <guang.alex.yang@gmail.com>
2017-04-21 00:02:12 -0700

    Setup AngularJS as front-end framework
    1. Installed angular, angular-route dependencies
    2. Created first draft of the login page
    TODO:
    - Hook up UI events with controller
    - Implemenet client logic to follow designed RESTful API for login workflow

--- webroot/app.js
+++ webroot/app.js
@@ -5,25 +5,22 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
+// configure express app
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
 
-app.use('/', index);
-app.use('/users', users);
+// configure routes
+// var index = require('./routes/index');
+// app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,7 +28,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
